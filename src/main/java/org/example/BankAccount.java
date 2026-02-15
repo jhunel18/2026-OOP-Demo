2e9b5020f8ba4b47c275fbd3e9a4b3e0c5eafc88
jhnl18 <jhunelpenaflorida@gmail.com>
2026-02-15 23:57:17 +0800

    Initial commit

---
+++ src/main/java/org/example/BankAccount.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class BankAccount {
+    private double balance;
+
+    public void deposit(double amount){
+        if(amount > 0){
+            balance += amount;
+        }
+    }
+    public double getBalance(){
+        return balance;
+    }
+}
---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Student {
+    private String name;
+
+    public void getName(String name){
+        this.name = name;
+    }
+
+    public String showName(){
+        return name;
+    }
+}
